151c22cd31b1e53dc9a1043eaee112a8d48d9882
tongying <283665752@qq.com>
2018-06-30 17:08:06 +0800

    upload

---
+++ express_server.js
@@ -0,0 +1,36 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var multer = require("multer");
+var path = require('path');
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function(req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`)
+    }
+})
+var upload = multer({ storage: storage });
+var app = express();
+app.use('/',express.static('dist/'));
+app.use('/upload', express.static(path.join(__dirname, './uploads/')))
+//var cpUpload = upload.fields([{ name: 'imgfile', maxCount: 12 }])
+app.post('/uploadimg', upload.array('imgfile', 40), function(req, res, next) {
+    var files = req.files
+    console.log(files);
+    if (!files[0]) {
+        res.send('error');
+    } else {
+        let result = {
+            hasError:false,
+            message:'上传成功',
+            data:files
+        }
+        res.send(result);
+    }
+})
+
+var server = app.listen(8999, 'localhost', function() {
+    console.log('server is running at port 9999...');
+});
